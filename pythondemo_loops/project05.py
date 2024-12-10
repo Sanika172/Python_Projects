72b2ba4d91c02579afd6a54913e074e107f68ec5
Sanika172 <sanuchougule172@gmail.com>
2024-12-10 14:21:02 +0530

    Initial commit

--- pythondemo_loops/project05.py
+++ pythondemo_loops/project05.py
@@ -4,3 +4,4 @@ print("table of", n, "is")
 
 for i in range(1, 11):
     print(n * i)
+                                                                                                                                                                                                                                                                  
